41f1bb8397b8ccd646a03b11d7b8e74fabe43702
Monmohan <monmohan@gmail.com>
2013-06-29 14:04:03 +0530

    Fortify test cases, use assert node module

--- tests/TestAVL.js
+++ tests/TestAVL.js
@@ -1,4 +1,4 @@
-var AVL = require("../src/AVLTree.js");
+var AVL = require("../src/AVLTree.js"),assert=require('assert');
 
 (function () {
     var bt = new AVL();
@@ -7,22 +7,18 @@ var AVL = require("../src/AVLTree.js");
         console.log("INSERT AND AVL BALANCE")
         bt.insert(16).insert(7).insert(25).insert(26).insert(39)
             .insert(13).insert(15).insert(29).insert(35).insert(12).insert(55).insert(11);
-        bt.checkInvariants(bt.root);
-
-    }
-
-    function testRotate() {
-        console.log(bt.root)
-        bt.rotate(bt.search(39), 'r');
-        console.log(bt.root)
-        bt.rotate(bt.search(29), 'l');
-        console.log(bt.root)
-
+        assert.doesNotThrow(function(){bt.checkInvariants(bt.root);},
+            "AVL Tree property violated")
+        assert.strictEqual(bt.search(15).item,15);
+        assert.strictEqual(bt.successor(15).item,16);
+        assert.strictEqual(bt.predecessor(35).item,29);
+        assert.strictEqual(bt.min().item,7);
+        assert.strictEqual(bt.max().item,55);
+        console.log(bt.root);
     }
 
     testInsertWithRebalance();
-    console.log(bt.root)
-    //testRotate();
-    //bt.checkInvariants(bt.root,true);
+
+
 
 })();
