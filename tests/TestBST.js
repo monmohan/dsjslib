e57e87ff8f0e47b435e1012f009d7c646959295d
Monmohan <monmohan@gmail.com>
2013-06-29 13:57:20 +0530

    Fortify test cases, use assert node module

--- tests/TestBST.js
+++ tests/TestBST.js
@@ -1,4 +1,4 @@
-var BinarySearchTree = require("../src/BinarySearchTree.js");
+var BinarySearchTree = require("../src/BinarySearchTree.js"),assert=require('assert');
 (function () {
     var bt = new BinarySearchTree();
 
@@ -6,6 +6,11 @@ var BinarySearchTree = require("../src/BinarySearchTree.js");
         console.log("INSERT AND TRAVERSE")
         bt.insert(16).insert(7).insert(25).insert(26).insert(39)
             .insert(13).insert(15).insert(29).insert(35).insert(12).insert(55).insert(11);
+        assert.strictEqual(bt.search(29).item,29);
+        assert.strictEqual(bt.search(7).item,7);
+        assert.strictEqual(bt.search(16).item,16);
+        assert.strictEqual(bt.search(12).item,12);
+        
         bt.traverse(function (node) {
             console.log(node.item)
         });
@@ -14,42 +19,36 @@ var BinarySearchTree = require("../src/BinarySearchTree.js");
 
     function testMinMax() {
         console.log("FIND min max")
-        console.log("min " + bt.min().item);
-        console.log("min " + bt.max().item);
+        assert.strictEqual(bt.min().item,7);
+        assert.strictEqual(bt.max().item,55);
     }
 
-    function testSearch() {
-        console.log("SEARCH")
-        var f = bt.search(29);
-        console.log("node " + f + " " + f ? f.item : "");
-    }
-
+    
     function testSuccPre() {
         console.log("SUCCESSOR AND PREDECESSOR")
-        console.log("successor of 26 = " + bt.successor(26).item);
-        console.log("pre of 11 = " + bt.predecessor(11).item);
+        assert.strictEqual(bt.successor(26).item,29);
+        assert.strictEqual(bt.successor(55),null);
+        assert.strictEqual(bt.successor(12).item,13);
+        assert.strictEqual(bt.predecessor(11).item,7);
+        assert.strictEqual(bt.predecessor(7),null);
+        assert.strictEqual(bt.predecessor(35).item,29);
     }
 
     function testDel() {
         console.log("DELETE")
         bt.delete(29);
-        bt.traverse(
-            function (node) {
-                console.log(node.item)
-            }
-        );
+        assert.strictEqual(bt.search(29),null);
         bt.delete(13);
-        bt.traverse(function (node) {
-            console.log(node.item)
-        });
+        assert.strictEqual(bt.search(13),null);
+
     }
 
     (function testBSTfuncs() {
         testInsert();
         testMinMax();
-        testSearch();
         testSuccPre();
         testDel();
+        //pretty print the tree
         console.log(bt.root);
     })();
 
