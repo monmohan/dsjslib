8af1e0ba581f9325af9124d30bcfc8893a9936d7
Monmohan <monmohan@gmail.com>
2013-08-08 14:23:57 +0530

    update index.js for exporting classes

--- tests/TestGeneral.js
+++ tests/TestGeneral.js
@@ -1,19 +1,46 @@
-function Class1() {
-    this.outer = 1
-    this.SubClass = function () {
-        this.inner = 1;
-    }
-    var mkNode = function (foo) {
-        return {'insert':function () {
-            return mkNode(foo)
-        }, "x":foo}
-    }
-    var obj = mkNode("beta");
-    console.log(obj.insert())
+(function()
+{var foo={
+    main:function(i){
+        console.log('main called'+i)
+    },
+    prop:'val',
+    fn2:function(x){
+        console.log('fn2 called')
+    },
+    objprop:{'k':1,'v':'v'}
 }
 
-(function () {
-    var o = new Class1();
-    var i = new o.SubClass();
+var common={
+    intercepted:function (obj, iFn, thisArg) {
+        function createMask(oldFn, iFn) {
+            return function () {
+                var mask = iFn.apply(thisArg || obj, Array.prototype.slice.apply(arguments));
+                if (mask) {
+                    oldFn.apply(obj, Array.prototype.slice.apply(arguments));
+                }
+            };
+        }
 
+        for (var fn in obj) {
+            if (obj.hasOwnProperty(fn) && typeof obj[fn] === 'function'
+                && typeof obj[fn].mask !== 'function'/*single mask only*/) {
+                obj[fn] = createMask(obj[fn], iFn);
+                obj[fn].mask = iFn;
+            }
+
+        }
+
+    },
+    test:function(i1,i2){
+        console.log('interceptor called '+i1+i2)
+        return true
+    }
+
+}
+    common.intercepted(foo,common.test,common);
+foo.main('a','b');
+foo.fn2(11,12);
+    common.intercepted(foo,common.test,common);
+    foo.main('a','b');
+    foo.fn2(11,12);
 })();
