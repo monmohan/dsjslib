6e9da7bed73f3185f0866e4991b2b7d10d28f3b4
Monmohan <monmohan@gmail.com>
2014-03-03 15:43:20 +0530

    Graph support :Shortest path on weighted undirected graph. simple utilities to print vertex and parent. list all vertices in BFS traversal order. TODO: Only primitive types supported for vertices

--- tests/TestDiGraph.js
+++ tests/TestDiGraph.js
@@ -5,13 +5,33 @@ var DiGraph = require("../lib/DiGraph.js"), util = require("util"), assert = req
           udg.addEdge(1,2);
           udg.addEdge(1,3);
           udg.addEdge(2,3);
-          udg.print();
+          udg.utils().printBFSTraversal();
           udg.addEdge(2,4);
           udg.addEdge(4,5);
           udg.addEdge(1,5);
           var path=udg.getShortestPath(1,5);
+          assert.deepEqual(path,['5','1']);
           console.log(path);
       }
+
+    function testShortestPathDirected(){
+        var dg=new DiGraph({'directed':true});
+        dg.addEdge("A","B");
+        dg.addEdge("A","C");
+        dg.addEdge("B","C");
+        dg.addEdge("B","E");
+        dg.addEdge("C","D");
+        dg.addEdge("E","F");
+        dg.addEdge("D","A");
+        dg.addEdge("D","G");
+        dg.addEdge("D","F");
+        dg.addEdge("F","G");
+        assert.deepEqual(dg.utils().listVerticesBFS(),[ 'A', 'B', 'C', 'E', 'D', 'F', 'G' ]);
+        var path=dg.getShortestPath("A","F");
+        assert.deepEqual(path,[ 'F', 'E', 'B', 'A' ]);
+        console.log(path);
+    }
     simpleTest()
+    testShortestPathDirected()
 
 })();
